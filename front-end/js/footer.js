98455097c71616bf7a07d754f00120282ca98f05
VanDinhDev <dtc225180066@ictu.edu.vn>
2025-09-06 18:43:23 +0700

    update

---
+++ front-end/js/footer.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    // Load the footer
+    fetch('components/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.body.insertAdjacentHTML('beforeend', data);
+        })
+        .catch(error => console.error('Error loading footer:', error));
+});
