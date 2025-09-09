69efe87d804677b96ab932b1e2c4192e670fa9a1
VanDinhDev <dtc225180066@ictu.edu.vn>
2025-09-09 12:28:35 +0700

    update user-serviec

---
+++ back-end/user-service/src/main/java/vandinh/ictu/user_service/common/enums/Gender.java
@@ -0,0 +1,5 @@
+package vandinh.ictu.user_service.common.enums;
+
+public enum Gender {
+    MALE, FEMALE, OTHER
+}
---
+++ back-end/user-service/src/main/java/vandinh/ictu/user_service/services/UserService.java
@@ -0,0 +1,19 @@
+package vandinh.ictu.user_service.services;
+
+import vandinh.ictu.user_service.dto.request.CreateUserRequest;
+import vandinh.ictu.user_service.dto.request.UpdateUserRequest;
+import vandinh.ictu.user_service.dto.request.UserPasswordRequest;
+import vandinh.ictu.user_service.dto.response.UserPageResponse;
+import vandinh.ictu.user_service.dto.response.UserResponse;
+
+public interface UserService {
+    UserPageResponse getAllUser(String keyword, String sort, int page, int size);
+    UserResponse getUserById(long id);
+    UserResponse getUserByUsername(String username);
+    UserResponse getUserByEmail(String email);
+    long addUser(CreateUserRequest req);
+    void updateUser(UpdateUserRequest req);
+    void changePassword(UserPasswordRequest req);
+    void deleteUser(long id);
+
+}
