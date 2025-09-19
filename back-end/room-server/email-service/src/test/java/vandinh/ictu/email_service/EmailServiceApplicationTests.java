d1e388cdef49066a128eba009809761a591f091d
VanDinhDev <dtc225180066@ictu.edu.vn>
2025-09-19 11:09:32 +0700

    add module email-service, notification-service

--- back-end/room-server/email-service/src/test/java/vandinh/ictu/email_service/EmailServiceApplicationTests.java
+++ back-end/room-server/email-service/src/test/java/vandinh/ictu/email_service/EmailServiceApplicationTests.java
@@ -1,4 +1,4 @@
-package vamdinh.ictu.email_service;
+package vandinh.ictu.email_service;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -6,8 +6,8 @@ import org.springframework.boot.test.context.SpringBootTest;
 @SpringBootTest
 class EmailServiceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
--- back-end/room-server/notification-service/src/main/java/vandinh/ictu/notification_service/NotificationServiceApplication.java
+++ back-end/room-server/notification-service/src/main/java/vandinh/ictu/notification_service/NotificationServiceApplication.java
@@ -1,4 +1,4 @@
-package vamdinh.ictu.notification_service;
+package vandinh.ictu.notification_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class NotificationServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(NotificationServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(NotificationServiceApplication.class, args);
+    }
 
 }
