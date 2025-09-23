dc215811de04baa072b7a93a829700f961ad8f4c
VanDinhDev <dtc225180066@ictu.edu.vn>
2025-09-23 14:57:15 +0700

    delete email-service

--- room-server/email-service/src/main/java/vandinh/ictu/email_service/exception/ForBiddenException.java
+++
@@ -1,7 +0,0 @@
-package vandinh.ictu.email_service.exception;
-
-public class ForBiddenException extends RuntimeException {
-    public ForBiddenException(String message) {
-        super(message);
-    }
-}
--- room-server/email-service/src/main/java/vandinh/ictu/email_service/repositories/EmailLogRepository.java
+++
@@ -1,9 +0,0 @@
-package vandinh.ictu.email_service.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-import vandinh.ictu.email_service.models.EmailLog;
-
-@Repository
-public interface EmailLogRepository extends JpaRepository<EmailLog, Long> {
-}
