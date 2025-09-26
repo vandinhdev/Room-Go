2d1ac308aa5ec8d25023d1eb5318a4e9d2611db4
Duy Trần <duytn2k4@gmail.com>
2025-09-26 23:03:12 +0700

    update favourite

--- front-end/js/header.js
+++ front-end/js/header.js
@@ -81,4 +81,24 @@ function initializeHeader() {
             });
         })
         .catch(error => console.error('Error loading provinces:', error));
+    
+    const favoriteBtn = document.getElementById("favourite-btn");
+    const savedPopup = document.querySelector(".favourite-room");
+
+    if (favoriteBtn && savedPopup) {
+        favoriteBtn.addEventListener("click", (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            savedPopup.style.display =
+                savedPopup.style.display === "block" ? "none" : "block";
+        });
+
+        document.addEventListener("click", (e) => {
+            if (!favoriteBtn.contains(e.target) && !savedPopup.contains(e.target)) {
+                savedPopup.style.display = "none";
+            }
+        });
+    }
 }
+
+
